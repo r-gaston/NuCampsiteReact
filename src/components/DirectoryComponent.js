04ddd8956c6e9a3f07ec48f564c6d0175a78d9f5
r-gaston <rgaston001@columbus.rr.com>
2021-10-08 22:09:28 -0400

    Components Part 2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,43 +1,35 @@
 import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: [
-                {
-                    id:0,
-                    image: 'assets/images/react-lake.jpg',
-                    elevation: 1233,
-                    description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
-                },
-                {
-                    id: 1,
-                    name: 'Chrome River Campground ',
-                    image: 'assets/images/chrome-river.jpg',
-                    elevation: 877,
-                    description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
-                  },
-                  {
-                      id: 2,
-                      name: 'Breadcrumb Trail Campground',
-                      image: 'assets/images/breadcrumb-trail.jpg',
-                      elevation: 2901,
-                      description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
-                  },
-                  {
-                      id: 3,
-                      name: 'Redux Woods Campground',
-                      image: 'assets/images/redux-woods.jpg',
-                      elevation: 42,
-                      description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
-                  }
-            ],
+          selectedCampsite:null
         };
     }
 
+    onCampsiteSelect(campsite) {
+        this.setState({selectedCampsite: campsite});
+    }
+
+    renderSelectedCampsite(campsite) {
+        if(campsite) {
+            return (
+                <Card>
+                    <CardImg top scr={campsite.name} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return <div />;
+    }
+
     render() {
-        const directory = this.state.campsites.map(campsites => {
+        const directory = this.props.campsites.map(campsites => {
         return (
             <div key={campsites.id} className="col">
                 <img src={campsites.image} alt={campsites.name} />
